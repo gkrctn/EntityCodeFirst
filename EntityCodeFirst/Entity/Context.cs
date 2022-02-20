66f4e7e54b379bea5af8391d35fe3d0d715357a0
gkrctn <gkr.cetin@gmail.com>
2022-02-20 22:24:55 +0300

    Add project files.

---
+++ EntityCodeFirst/Entity/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityCodeFirst.Entity
+{
+    class Context : DbContext
+    {
+        public DbSet<Urunler> Urunlers { get; set; }
+        public DbSet<Kategori> Kategoris { get; set; }
+        public DbSet<Musteri> Musteris { get; set; }
+
+    }
+}
